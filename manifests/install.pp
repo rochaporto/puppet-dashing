220fbf7bad63d8c80191db858213f8e162fe68b9
Ricardo Rocha <rocha.porto@gmail.com>
2014-03-13 13:10:19 +1300

    added install requirements for ubuntu precise.
    precise only has ruby 1.8.x, 1.9.x is required for dashing. some extra
    packages are needed to get puppet still playing nicely with the new
    ruby.

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,48 +10,6 @@
 #
 class dashing::install {
 
-  include apt
-
-  apt::source {'ruby-ng':
-    location    => 'http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu',
-    repos       => 'main',
-    release     => 'precise',
-    key         => 'C3173AA6',
-    key_server  => 'keyserver.ubuntu.com',
-    include_src => false,
-  }
-
-  if !defined(Package['ruby1.9.3']) {
-    package {'ruby1.9.3':
-      ensure  => installed,
-      require => Apt::Source['ruby-ng'],
-    }
-    package {'ruby-switch':
-      ensure  => installed,
-      require => Package['ruby1.9.3'],
-    }
-  }
-
-  if !defined(Package['rubygems']) {
-    package {'rubygems':
-      ensure  => installed,
-      require => Package['ruby1.9.3'],
-    }
-  }
-
-  if !defined(Package['ruby-bundler']) {
-    package {'ruby-bundler':
-      ensure => installed,
-      require => Package['ruby1.9.3'],
-    }
-  }
-
-  if !defined(Package['nodejs']) {
-    package {'nodejs':
-      ensure => installed,
-    }
-  }
-
   file {$dashing::dashing_basepath:
     ensure => directory,
     owner  => $dashing::run_user,
@@ -62,6 +20,52 @@ class dashing::install {
   package {$dashing::dashing_package_name:
     ensure   => $dashing::package_status,
     provider => 'gem',
+    require  => Package['rubygems'],
+  }
+
+  if !defined(Package['nodejs']) {
+    package {'nodejs':
+      ensure => installed,
+    }
+  }
+
+  if !defined(Package['rubygems']) {
+    package {'rubygems':
+      ensure  => installed,
+    }
+  }
+
+  if !defined(Package['ruby-bundler']) {
+    package {'ruby-bundler':
+      ensure => installed,
+    }
+  }
+
+  # ruby version should be >= 1.9.x (1.8.x default in ubuntu precise)
+  # it gets messy as puppet has a couple of required gems (augeas)
+  if $::lsbdistcodename == 'precise' {
+
+    if !defined(Package['ruby1.9.3']) {
+      package {'ruby1.9.3':
+        ensure => installed,
+        notify => Exec['update-ruby-1.9.3'],
+      }
+    }
+
+    if !defined(Package['libaugeas-ruby1.9.1']) {
+      package {'libaugeas-ruby1.9.1':
+        ensure  => installed,
+        require => Package['ruby1.9.3'],
+      }
+    }
+
+    exec {'update-ruby-1.9.3':
+      command     => '/usr/sbin/update-alternatives --set ruby /usr/bin/ruby1.9.1; /usr/sbin/update-alternatives --set gem /usr/bin/gem1.9.1',
+      refreshonly => true,
+    }
+
+    Package['libaugeas-ruby1.9.1'] -> Package['rubygems'] ~> Exec['update-ruby-1.9.3'] -> Package[$dashing::dashing_package_name]
+
   }
 
 }
